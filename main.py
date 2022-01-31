670f6e085a671109964a174d2826b9770c647539
Bharti kumari <bhartikumari20@navgurukul.org>
2022-01-31 22:55:53 +0530

    modify checkout function

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import json
 import re
 import random
 
-motorCy_parking_charge = 30
-car_parking_charge = 50
-bus_parking_charge = 70
 main_dic = {'vehicle_details': []}
 
-# total_parking_slots = {'motorcycle_spots': 20, 'car_spots':20,'bus_spots':20 }
-m_location_tokens = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-c_location_tokens = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-b_location_tokens = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
 def singup():
-    username = input('Please enter your name ')
-    password = input('Please enter password ')
+    username = input('Please enter your name  ')
+    password = input('Please enter password  ')
     confirm_password = input('Re-enter your password  ')
     dic  = {'user':[]}
     userData = {'username':"", 'password':""}
@@ -61,8 +53,8 @@ def singup():
         print('Both password are not same')
 
 def agent_login():
-    user_name = input('Enter your username ')
-    password = input('Enter your password  ')
+    user_name = input('Enter your username  ')
+    password = input('Enter your password   ')
     jsonData = open("agentdetails.json", "r")
     allData = json.load(jsonData)
     i = 0
@@ -73,7 +65,6 @@ def agent_login():
         i = i +1  
     else:
         print("Invalid username and password")
-
 
 def read_json(fname):
     if fname == "total_parking_slots.json":
@@ -86,24 +77,26 @@ def read_json(fname):
         location_tokens = json.load(j_data)
         j_data.close()
         return location_tokens
-    else:
+    elif fname == 'vehicledetails.json':
         json_data = open('vehicledetails.json', 'r')
         p_data = json.load(json_data)
         json_data.close()
         return p_data
 
-def write_in_json(data):
-    f_data = read_json('vehicledetails.json')
-    f = open('vehicledetails.json', 'w')
-    if len(f_data) == 0:
-        main_dic['vehicle_details'].append(data)
-        json.dump(main_dic, f)
-        f.close()
-    else:
-        f = open('vehicledetails.json', 'a')
+def write_in_json(fname, data):
+    if fname == 'vehicledetails.json':
         all_d = read_json('vehicledetails.json')
         all_d['vehicle_details'].append(data)
-        json.dump(all_d, f )
+        f = open(fname, 'w')
+        json.dump(all_d, f , indent = 2)
+        f.close()
+    elif fname == 'location_tokens.json':
+        f = open(fname, 'w')
+        json.dump(data, f, indent=2)
+        f.close()
+    else:
+        f = open(fname, 'w')
+        json.dump(data, f)
         f.close()
 
 def checkIn():
@@ -125,11 +118,15 @@ def checkIn():
             parking_slot = 'motorcycle_spots'
             sub_motorCy_s = total_parking_slots['motorcycle_spots'] -1
             total_parking_slots['motorcycle_spots'] = sub_motorCy_s
+
+            write_in_json('total_parking_slots.json',total_parking_slots)
+
             token_no = random.choice(p_location_tokens["m_location_tokens"])
-            m_location_tokens.remove(token_no)
-            slip_print('motorcycle',vehicle_no, date, time, parking_slot,token_no, motorCy_parking_charge)
+            p_location_tokens['m_location_tokens'].remove(token_no)
+            slip_print('motorcycle',vehicle_no, date, time, parking_slot,token_no, 30)
+            write_in_json('location_tokens.json', p_location_tokens)
             dic = {'vehicle_type': 'motorcycle', 'Mobile_no': mobile_no,'vehicle_no': vehicle_no, 'Date': date, 'Time': time, 'parking_slot': parking_slot}
-            write_in_json(dic)
+            write_in_json('vehicledetails.json',dic)
 
     elif vehicle_type == '2':
         if total_parking_slots['car'] == '0':
@@ -138,11 +135,13 @@ def checkIn():
             parking_slot = 'car_spots'
             sub_car_s = total_parking_slots['car_spots'] -1
             total_parking_slots['car_spots'] = sub_car_s
-            token_no = random.choice(c_location_tokens)
-            c_location_tokens.remove(token_no)
+            write_in_json('total_parking_slots.json',total_parking_slots)
+            token_no = random.choice(p_location_tokens['c_location_tokens'])
+            p_location_tokens['c_location_tokens'].remove(token_no)
+            slip_print('Car',vehicle_no, date, time, parking_slot,token_no, 50)
+            write_in_json('location_tokens.json', p_location_tokens)
             dic = {'vehicle_type': 'Car', 'Mobile_no': mobile_no,'vehicle_no': vehicle_no, 'Date': date, 'Time': time, 'parking_slot': parking_slot}
-            write_in_json(dic)
-            slip_print('Car',vehicle_no, date, time, parking_slot,token_no, car_parking_charge)
+            write_in_json('vehicledetails.json',dic)
 
     elif vehicle_type == '3':
         if total_parking_slots['bus'] == '0':
@@ -151,37 +150,35 @@ def checkIn():
             parking_slot = 'bus_spots'
             sub_bus_s = total_parking_slots['bus_spots'] -1
             total_parking_slots['bus_spots'] = sub_bus_s
-            token_no = random.choice(b_location_tokens)
-            b_location_tokens.remove(token_no)
+            write_in_json('total_parking_slots.json',total_parking_slots)
+            token_no = random.choice(p_location_tokens['b_location_tokens'])
+            p_location_tokens['b_location_tokens'].remove(token_no)
+            slip_print("Bus",vehicle_no, date, time, parking_slot,token_no, 70)
+            write_in_json('location_tokens.json', p_location_tokens)
             dic = {'vehicle_type': 'Bus', 'Mobile_no': mobile_no,'vehicle_no': vehicle_no, 'Date': date, 'Time': time, 'parking_slot': parking_slot}
-            write_in_json(dic)
-            slip_print("Bus",vehicle_no, date, time, parking_slot,token_no, bus_parking_charge)
-
+            write_in_json('vehicledetails.json', dic)
     else:
         print('Invalid input')
 
 def checkout():
-    print('Vehicle and Charge -  1.Motorcycle: 30   2.Car: 50   3.Bus: 70')
-    vehicle_type = input('Enter veicle type 1.motorcycle 2. car 3. buses')
-    tokenData = read_json("location_tokens.json")
-
-    if vehicle_type == '1':
-        token = int(input('Please give your token number'))
-        tokenData["m_location_tokens"].append(token)
-
-    elif vehicle_type == '2':
-        token = int(input('Please give your token number'))
-        tokenData["c_location_tokens"].append(token)
-    
-    elif vehicle_type == '3':
-        token = int(input('Please give your token number'))
-        tokenData["b_location_tokens"].append(token)
+    vehicle_type = input('Enter vehicle type motorcycle/car/buses  ')
+    tokens = read_json("location_tokens.json")
+    token = int(input('Please give your token number'))
+    if vehicle_type == 'motorcycle':
+        tokens["m_location_tokens"].append(token)
+        write_in_json('location_tokens.json', tokens)
+    elif vehicle_type == 'car':
+        tokens["c_location_tokens"].append(token)
+        write_in_json('location_tokens.json', tokens)
+    elif vehicle_type == 'buses':
+        tokens["b_location_tokens"].append(token)
+        write_in_json('location_tokens.json', tokens)
     else:
         print('Enter valid token')
 
 def slip_print(vehicle_t, vehicles_n, date, time, parking_slot, token, amount):
     print()
-    print('         Parking Slip            ')
+    print('        Parking Slip            ')
     print()
     print('vehicle_type : ', vehicle_t)
     print('Vehicle number: ', vehicles_n)
@@ -192,7 +189,6 @@ def slip_print(vehicle_t, vehicles_n, date, time, parking_slot, token, amount):
     print('Amount :', amount)
     print()
 
-
 def main():
     parking_spots = ['motorcycle_spots', 'car_spots','bus_spots' ]
     agent_input = input('For signup press 1 or For login press 2.  ')
@@ -201,7 +197,7 @@ def main():
     else:
         agent_login()
         print()
-        choose = input('Select in/out')
+        choose = input('Select in/out ')
         if choose == 'in':
             checkIn()
         elif choose == 'out':
@@ -209,7 +205,6 @@ def main():
         else:
             print('Enter valid input')
 main()
-    
 
 
 
