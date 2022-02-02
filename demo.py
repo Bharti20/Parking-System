8f19ca8446608993086a8fc488336725444d8520
Bharti kumari <bhartikumari20@navgurukul.org>
2022-02-02 22:35:45 +0530

    add loop

--- demo.py
+++
@@ -1,8 +0,0 @@
-import random
-a = {"list1": [1, 2, 3, 4, 5, 6]}
-a = random.choice(a['list1'])
-print(a)
-# list1.remove(a)
-# print(a)
-# print(list1)
-# i = i + 1
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 import random
 
 main_dic = {'vehicle_details': []}
+all_tokens = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 def singup():
     username = input('Please enter your name  ')
@@ -22,7 +23,7 @@ def singup():
                     while i<len(all_data["user"]):
                         a=(all_data["user"][i])
                         if a["username"] == username:
-                            print("alredy exist")
+                            print("User alredy exist")
                             check = 1
                             break
                         i=i+1
@@ -34,7 +35,7 @@ def singup():
                     dic['user'].append(userData)
                     print(dic)
                     json_file = open("agentdetails.json", "w")
-                    json.dump(dic, json_file)
+                    json.dump(dic, json_file, indent = 2)
                     json_file.close()
                     print("congrarts", username, "you are Signed Up Successfully")
                 
@@ -42,7 +43,7 @@ def singup():
                     userData = {'username': username, 'password': password}
                     all_data['user'].append(userData)
                     my_data = open("agentdetails.json", "w")
-                    json.dump(all_data, my_data)
+                    json.dump(all_data, my_data, indent = 2)
                     my_data.close()
                     print("congrarts", username, "you are Signed Up Successfully")
             else:
@@ -65,14 +66,10 @@ def agent_login():
         i = i +1  
     else:
         print("Invalid username and password")
+        return 'invalid'
 
 def read_json(fname):
-    if fname == "total_parking_slots.json":
-        j_data = open("total_parking_slots.json", "r")
-        t_p_slots = json.load(j_data)
-        j_data.close()
-        return t_p_slots
-    elif fname == "location_tokens.json":
+    if fname == "location_tokens.json":
         j_data = open("location_tokens.json", "r")
         location_tokens = json.load(j_data)
         j_data.close()
@@ -94,33 +91,25 @@ def write_in_json(fname, data):
         f = open(fname, 'w')
         json.dump(data, f, indent=2)
         f.close()
-    else:
-        f = open(fname, 'w')
-        json.dump(data, f)
-        f.close()
 
 def checkIn():
     print()
     print('Welcome to Bharti Mall parking')
     print()
-    print('Vehicle and Charge -  1.Motorcycle: 30   2.Car: 50   3.Bus: 70')
-    vehicle_type = input('Enter veicle type 1.motorcycle 2. car 3. buses')
+    print('Vehicles and Charge -  1.Motorcycle: 30   2.Car: 50   3.Bus: 70')
+    print()
+    vehicle_type = input('Enter veicle type 1.motorcycle 2. car 3. buses    ')
     vehicle_no = input('Enter vehicle number...')
     mobile_no = int(input('Enter your mobile number  '))
-    date = input('Enter date' )
-    time = input('Enter in time')
-    total_parking_slots = read_json("total_parking_slots.json")
+    date = input('Enter date    ' )
+    time = input('Enter in time ')
     p_location_tokens = read_json("location_tokens.json")
     if vehicle_type == '1':
-        if total_parking_slots['motorcycle_spots'] == '0':
-            return 'Sorry, Motorcycle slot is full Now. You can try somtime later'
+        if len(p_location_tokens["m_location_tokens"]) == 0:
+            print('Sorry, Motorcycle slot is full Now. You can try somtime later')
+            return
         else:
             parking_slot = 'motorcycle_spots'
-            sub_motorCy_s = total_parking_slots['motorcycle_spots'] -1
-            total_parking_slots['motorcycle_spots'] = sub_motorCy_s
-
-            write_in_json('total_parking_slots.json',total_parking_slots)
-
             token_no = random.choice(p_location_tokens["m_location_tokens"])
             p_location_tokens['m_location_tokens'].remove(token_no)
             slip_print('motorcycle',vehicle_no, date, time, parking_slot,token_no, 30)
@@ -129,13 +118,11 @@ def checkIn():
             write_in_json('vehicledetails.json',dic)
 
     elif vehicle_type == '2':
-        if total_parking_slots['car'] == '0':
-            return 'Sorry, car slot is full Now. You can try somtime later'
+        if len(p_location_tokens["c_location_tokens"]) == 0:
+            print('Sorry, car slot is full Now. You can try somtime later')
+            return
         else:
             parking_slot = 'car_spots'
-            sub_car_s = total_parking_slots['car_spots'] -1
-            total_parking_slots['car_spots'] = sub_car_s
-            write_in_json('total_parking_slots.json',total_parking_slots)
             token_no = random.choice(p_location_tokens['c_location_tokens'])
             p_location_tokens['c_location_tokens'].remove(token_no)
             slip_print('Car',vehicle_no, date, time, parking_slot,token_no, 50)
@@ -144,13 +131,11 @@ def checkIn():
             write_in_json('vehicledetails.json',dic)
 
     elif vehicle_type == '3':
-        if total_parking_slots['bus'] == '0':
-            return 'Sorry, Bus slot is full Now. You can try somtime later'
+        if len(p_location_tokens["b_location_tokens"]) == 0:
+            print('Sorry, Bus slot is full Now. You can try somtime later')
+            return
         else:
             parking_slot = 'bus_spots'
-            sub_bus_s = total_parking_slots['bus_spots'] -1
-            total_parking_slots['bus_spots'] = sub_bus_s
-            write_in_json('total_parking_slots.json',total_parking_slots)
             token_no = random.choice(p_location_tokens['b_location_tokens'])
             p_location_tokens['b_location_tokens'].remove(token_no)
             slip_print("Bus",vehicle_no, date, time, parking_slot,token_no, 70)
@@ -161,20 +146,37 @@ def checkIn():
         print('Invalid input')
 
 def checkout():
+    p_location_tokens = read_json("location_tokens.json")
     vehicle_type = input('Enter vehicle type motorcycle/car/buses  ')
     tokens = read_json("location_tokens.json")
-    token = int(input('Please give your token number'))
+    token = int(input('Please give your token number  '))
+    print()
     if vehicle_type == 'motorcycle':
-        tokens["m_location_tokens"].append(token)
-        write_in_json('location_tokens.json', tokens)
+        if token in all_tokens and token not in p_location_tokens["m_location_tokens"]:
+            tokens["m_location_tokens"].append(token)
+            write_in_json('location_tokens.json', tokens)
+            print()
+            print('Thank you for visit')
+        else:
+            print('Wrong Token, Please Talk with Manager')
     elif vehicle_type == 'car':
-        tokens["c_location_tokens"].append(token)
-        write_in_json('location_tokens.json', tokens)
+        if token in all_tokens and token not in p_location_tokens["c_location_tokens"]:
+            tokens["c_location_tokens"].append(token)
+            write_in_json('location_tokens.json', tokens)
+            print()
+            print('Thank you for visit')
+        else:
+            print('Wrong Token,  Please Talk with Manager')
     elif vehicle_type == 'buses':
-        tokens["b_location_tokens"].append(token)
-        write_in_json('location_tokens.json', tokens)
+        if token in all_tokens and token not in p_location_tokens["b_location_tokens"]:
+            tokens["b_location_tokens"].append(token)
+            write_in_json('location_tokens.json', tokens)
+            print()
+            print('Thank you for visit')
+        else:
+            print('Wrong token')
     else:
-        print('Enter valid token')
+        print('Enter valid token, Please Talk with Manager')
 
 def slip_print(vehicle_t, vehicles_n, date, time, parking_slot, token, amount):
     print()
@@ -195,17 +197,34 @@ def main():
     if agent_input == '1':
         singup()
     else:
-        agent_login()
+        outPut = agent_login()
         print()
-        choose = input('Select in/out ')
-        if choose == 'in':
-            checkIn()
-        elif choose == 'out':
-            checkout()
+        if outPut == 'invalid':
+            return
         else:
-            print('Enter valid input')
-main()
+            choose = input('Customer in/out. Select checkin/checkout ')
+            if choose == 'checkin':
+                checkIn()
+            elif choose == 'checkout':
+                checkout()
+            else:
+                print('Enter valid input') 
 
+            while True:
+                agent_choice = input('Agent, Do you want to stop working? yes/no    ')
+                if agent_choice == 'yes':
+                    break 
+                elif agent_choice == 'no':
+                    choose = input('Customer in/out. Select checkin/checkout ')
+                    if choose == 'checkin':
+                        checkIn()
+                    elif choose == 'checkout':
+                        checkout()
+                    else:
+                        print('Enter valid input')
+                else:
+                    print('Invalid input')
+main()
 
 
 
